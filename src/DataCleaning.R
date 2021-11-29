a0f74ee927c9be9c6ec65cff6cd8c937d503c69d
BryantR1 <bryant_tnk@hotmail.com>
2021-11-28 22:38:53 -0600

    1. Created 4 models: Correlation matrix top 8 vars, correlation marix no outliers, custom model, custom model no outliers.
    2. Predicted the price of the houses in the test data set.
    
    3. Created a folder for all the images that will be used in the presentation.
    
    4. Added code to the DataCleaning.R file which creates a cleaned dataframe for the test data set.

--- src/DataCleaning.R
+++ src/DataCleaning.R
@@ -11,6 +11,25 @@ working_ds <- data.frame(train_ds)
 # test_ds: The testing dataset that we will use to predict the accuracy of our model
 test_ds = read.csv("data/test.csv")
 
+#columns to drop
+# 1. get houses with all utilities: Utilities == AllPub
+# 2. Consider only residential zones: MSZoning != A, C, FV, I
+# Consider different  MSSubclass: Start with 1 story all ages, 1 story w/finished attic all ages,
+#     : MSSubClass == 20, 30, 40
+# 3. consider only normal house sales: SaleCondition == Normal
+drop_zones <- c('A','C (all)', 'FV', 'I')
+keep_subclass <- c(20, 30, 40)
+working_ds <- working_ds[working_ds['Utilities'] == 'AllPub', ]
+working_ds <- working_ds[!(working_ds$MSZoning %in% drop_zones), ]
+working_ds <- working_ds[(working_ds$MSSubClass %in% keep_subclass), ]
+# for 1 story houses, the general living area and the 1st floor square feet are the same
+working_ds$GrLivArea <- NULL
+# data still had some values with square feet on the second floor even after filtering for 1 story houses
+# remove them
+working_ds <- working_ds[working_ds$X2ndFlrSF == 0,]
+working_ds <- working_ds[(working_ds['SaleCondition'] == 'Normal'),]
+
+
 # before altering the data lets create a correlation matrix for all the variables
 check <- which(sapply(working_ds, is.numeric))
 cor_df <- colnames(working_ds)[check]
@@ -47,31 +66,11 @@ sortCor <- function(df) {
 # all of the variables sorted in descending order with their respective variables.
 cor_df <- sortCor(cor_df)
 
-## We will create a model based on the first 9 attributes of the
+## We will create a model based on the first 8 attributes of the
 ## sale price column of the sorted correlation matrix
 
 model_1_df <- cor_df[,((ncol(cor_df) - 1):ncol(cor_df))]
-model_1_attr <- model_1_df[[1]][1:10]
-#columns to drop
-# 1. get houses with all utilities: Utilities == AllPub
-# 2. Consider only residential zones: MSZoning != A, C, FV, I
-# Consider different  MSSubclass: Start with 1 story all ages, 1 story w/finished attic all ages,
-#     : MSSubClass == 20, 30, 40
-# 3. check condition1 and condition2, optimally we would want all houses to have 'normal' conditions
-#     as a nearby railroad or park may affect the price
-# 4. Consider GarageFinish as Finished only
-# 5. consider only normal house sales: SaleCondition == Normal
-drop_cols <- c('Street', 'Alley', 'KitchenAbvGr', 'KitchenQual')
-drop_basement <- c('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1', 'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF')
-drop_cols <- c(drop_cols, drop_basement)
-drop_zones <- c('A','C (all)', 'FV', 'I')
-keep_subclass <- c(20, 30, 40)
-working_ds[drop_cols] <- NULL
-working_ds <- working_ds[working_ds['Utilities'] == 'AllPub', ]
-working_ds <- working_ds[!(working_ds$MSZoning %in% drop_zones), ]
-working_ds <- working_ds[(working_ds$MSSubClass %in% keep_subclass), ]
-working_ds <- working_ds[(working_ds['SaleCondition'] == 'Normal'),]
-
+model_1_attr <- model_1_df[[1]][1:9]
 model_1_df <- data.frame(working_ds[model_1_attr])
 
 
@@ -81,29 +80,47 @@ working_ds <- data.frame(train_ds)
 # 1. get houses with all utilities: Utilities == AllPub
 # 2. Consider only residential zones: MSZoning != A, C, FV, I
 # Consider different  MSSubclass: Start with 1 story all ages, 1 story w/finished attic all ages,
-#   2 story all ages: MSSubClass == 20, 30, 40, 60, 70
-# 3. check condition1 and condition2, optimally we would want all houses to have 'normal' conditions
-#     as a nearby railroad or park may affect the price
-# 4. Consider GarageFinish as Finished only
-# 5. consider only normal house sales: SaleCondition == Normal
-
-drop_cols <- c('Street', 'Alley', 'KitchenAbvGr', 'KitchenQual')
-drop_basement <- c('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1', 'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF')
-drop_cols <- c(drop_cols, drop_basement)
+#     : MSSubClass == 20, 30, 40
+# 3. consider only normal house sales: SaleCondition == Normal
 drop_zones <- c('A','C (all)', 'FV', 'I')
-keep_subclass <- c(20, 30, 40, 60, 70)
-working_ds[drop_cols] <- NULL
+keep_subclass <- c(20, 30, 40)
 working_ds <- working_ds[working_ds['Utilities'] == 'AllPub', ]
 working_ds <- working_ds[!(working_ds$MSZoning %in% drop_zones), ]
 working_ds <- working_ds[(working_ds$MSSubClass %in% keep_subclass), ]
+# for 1 story houses, the general living area and the 1st floor square feet are the same
+working_ds$GrLivArea <- NULL
+# data still had some values with square feet on the second floor even after filtering for 1 story houses
+# remove them
+working_ds <- working_ds[working_ds$X2ndFlrSF == 0,]
 working_ds <- working_ds[(working_ds['SaleCondition'] == 'Normal'),]
 
-## these attributes contain the second floor square footage, we will remove this temporarily
-#model_attr <- c('LotArea', 'YearBuilt', 'TotalBsmtSF', 'X1stFlrSF', 'X2ndFlrSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath',
-#                'FullBath', 'HalfBath', 'BedroomAbvGr', 'Fireplaces', 'GarageCars', 'GarageArea', 'SalePrice')
-
-
-model_2_attr <- c('LotArea', 'YearBuilt', 'TotalBsmtSF', 'X1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath',
+model_2_attr <- c('LotArea', 'YearBuilt', 'TotalBsmtSF', 'X1stFlrSF', 'BsmtFullBath', 'BsmtHalfBath',
                 'FullBath', 'HalfBath', 'BedroomAbvGr', 'Fireplaces', 'GarageCars', 'GarageArea', 'SalePrice')
 
 model_2_df <- data.frame(working_ds[model_2_attr])
+
+# Next we will clean the testing dataset to estimate the values of the houses
+working_test_ds_2 <- data.frame(test_ds)
+#columns to drop
+# 1. get houses with all utilities: Utilities == AllPub
+# 2. Consider only residential zones: MSZoning != A, C, FV, I
+# Consider different  MSSubclass: Start with 1 story all ages, 1 story w/finished attic all ages,
+#     : MSSubClass == 20, 30, 40
+# 3. consider only normal house sales: SaleCondition == Normal
+drop_zones <- c('A','C (all)', 'FV', 'I')
+keep_subclass <- c(20, 30, 40)
+working_test_ds_2 <- working_test_ds_2[working_test_ds_2['Utilities'] == 'AllPub', ]
+working_test_ds_2 <- working_test_ds_2[!(working_test_ds_2$MSZoning %in% drop_zones), ]
+working_test_ds_2 <- working_test_ds_2[(working_test_ds_2$MSSubClass %in% keep_subclass), ]
+# for 1 story houses, the general living area and the 1st floor square feet are the same
+working_test_ds_2$GrLivArea <- NULL
+# data still had some values with square feet on the second floor even after filtering for 1 story houses
+# remove them
+working_test_ds_2 <- working_test_ds_2[working_test_ds_2$X2ndFlrSF == 0,]
+working_test_ds_2 <- working_test_ds_2[(working_test_ds_2['SaleCondition'] == 'Normal'),]
+
+model_2_attr <- c('LotArea', 'YearBuilt', 'TotalBsmtSF', 'X1stFlrSF', 'BsmtFullBath', 'BsmtHalfBath',
+                  'FullBath', 'HalfBath', 'BedroomAbvGr', 'Fireplaces', 'GarageCars', 'GarageArea')
+
+test_model_2_df <- data.frame(working_test_ds_2[model_2_attr])
+test_model_2_df <- na.omit(test_model_2_df)
--- src/main.R
+++ src/main.R
@@ -24,7 +24,7 @@ influentialPointsPlots(dfbetas_model_1_lm, model_1_df)
 
 ## On the relationship plot, we can see we need to convert some variables into factors to get an accurate linear model
 # get the name of the columns that we want to convert to factors
-dummy_vars <- c('OverallQual', 'GarageCars', 'FullBath', 'TotRmsAbvGrd')
+dummy_vars <- c('OverallQual', 'TotRmsAbvGrd', 'FullBath', 'GarageCars')
 dummy_df_1 <- dummy_cols(model_1_df, select_columns = dummy_vars, remove_selected_columns = TRUE)
 
 # create a linear model using the new dataframe 
@@ -81,10 +81,58 @@ influentialPointsPlots(dfbetas_model_1_transform, dummy_df_1_transform)
 # printing residual plots
 printPlots(dummy_model_1_transform)
 
+#############
+##Model 1 without outliers
+#############
+temp <- dfbetas_model_1_lm[[9]][2:length(dfbetas_model_1_lm[[9]])]
+temp_df <- model_1_df[rownames(model_1_df) %in% temp,]
+
+#outliers for 1st floor
+outliers_1 <- c(441,1224)
+#outliers for basement
+outliers_2 <- c(333,935,1224,441)
+# outliers for fullbath
+outliers_3 <- c(54,441)
+# outliers for garage area
+outliers_4 <- c(441)
+remove_outliers <- c(outliers_1,outliers_2,outliers_3,outliers_4)
+remove_outliers <- unique(remove_outliers)
+
+# create a new dataframe with the outlier observations removed
+model_1_df_outliers <- data.frame(model_1_df)
+model_1_df_outliers <- model_1_df_outliers[!rownames(model_1_df_outliers) %in% remove_outliers,]
+# plot the relationship after removing the outliers
+plotRelationship(model_1_df_outliers)
+# create a linear model
+model_1_lm_outliers <- lm(SalePrice ~ ., data = model_1_df_outliers)
+summary(model_1_lm_outliers)
+
+## once again create new plots for the transformed model
+# store the DFBETAS points
+dfbetas_model_1_outliers <- dfbetasPoints(model_1_lm_outliers)
+
+# Plot the influential points
+influentialPointsPlots(dfbetas_model_1_outliers, model_1_df_outliers)
+
+## get the name of the columns that we want to convert to factors
+dummy_vars <- c('OverallQual', 'TotRmsAbvGrd', 'FullBath', 'GarageCars' )
+dummy_df_1_outliers <- dummy_cols(model_1_df_outliers, select_columns = dummy_vars, remove_selected_columns = TRUE)
+
+# create a linear model using the new dataframe 
+dummy_model_1_outliers <- lm(SalePrice ~ ., data = dummy_df_1_outliers)
+summary(dummy_model_1_outliers)
+# remove collinearity
+colinearity_attr <- attributes(alias(dummy_model_1_outliers)$Complete)$dimnames[[1]]
+dummy_df_1_outliers <- dummy_df_1_outliers[!(names(dummy_df_1_outliers) %in% colinearity_attr)]
+# creating a new linear model without the variables that were linearly dependent
+dummy_model_1_outliers <- lm(SalePrice ~ ., data = dummy_df_1_outliers)
+summary(dummy_model_1_outliers)
 
 
 
-### Model 2
+#############
+##Model 2
+#############
 # plotting relationship between variables
 plotRelationship(model_2_df)
 # show the potential outliers in the model before creating dummy variables
@@ -105,7 +153,7 @@ dummy_df_2 <- dummy_cols(model_2_df, select_columns = dummy_vars, remove_selecte
 dummy_model_2 <- lm(SalePrice ~ ., data = dummy_df_2)
 summary(dummy_model_2)
 #check for vif values
-vif(dummy_model_2)
+#vif(dummy_model_2)
 ## we can see that we cannot calculate the VIFs because some of the variables are linearly dependent
 ## we will remove these variables and create a new linear model
 colinearity_attr <- attributes(alias(dummy_model_2)$Complete)$dimnames[[1]]
@@ -154,4 +202,66 @@ influentialPointsPlots(dfbetas_model_2_transform, dummy_df_2_transform)
 
 # printing residual plots
 printPlots(dummy_model_2_transform)
+
+
+#############
+##Model 2 without outliers
+#############
+temp <- dfbetas_model_2_lm[[2]][2:length(dfbetas_model_2_lm[[2]])]
+temp_df <- model_2_df[rownames(model_2_df) %in% temp,]
+
+#outliers for lot area
+outliers_1 <- c(54,314,1397,452,707,458,272,1288,1185,1447,1384,877)
+#outliers for basement
+outliers_2 <- c(935,1224,333, 441)
+# outliers for 1st floor
+outliers_3 <- c(1224,441)
+# outliers for fullbath
+outliers_4 <- c(54)
+# outliers for bedrooms
+outliers_5 <- c(54)
+# outliers for garage area
+outliers_6 <- c(441)
+remove_outliers <- c(outliers_1,outliers_2,outliers_3,outliers_4,outliers_5,outliers_6)
+remove_outliers <- unique(remove_outliers)
+# create a new dataframe with the outlier observations removed
+model_2_df_outliers <- data.frame(model_2_df)
+model_2_df_outliers <- model_2_df_outliers[!rownames(model_2_df_outliers) %in% remove_outliers,]
+# plot the relationship after removing the outliers
+plotRelationship(model_2_df_outliers)
+# create a linear model
+model_2_lm_outliers <- lm(SalePrice ~ ., data = model_2_df_outliers)
+summary(model_2_lm_outliers)
+## once again create new plots for the transformed model
+# store the DFBETAS points
+dfbetas_model_2_outliers <- dfbetasPoints(model_2_lm_outliers)
+
+# Plot the influential points
+influentialPointsPlots(dfbetas_model_2_outliers, model_2_df_outliers)
+
+## get the name of the columns that we want to convert to factors
+dummy_vars <- c('BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'Fireplaces', 'GarageCars')
+dummy_df_2_outliers <- dummy_cols(model_2_df_outliers, select_columns = dummy_vars, remove_selected_columns = TRUE)
+
+# create a linear model using the new dataframe 
+dummy_model_2_outliers <- lm(SalePrice ~ ., data = dummy_df_2_outliers)
+summary(dummy_model_2_outliers)
+# remove collinearity
+colinearity_attr <- attributes(alias(dummy_model_2_outliers)$Complete)$dimnames[[1]]
+dummy_df_2_outliers <- dummy_df_2_outliers[!(names(dummy_df_2_outliers) %in% colinearity_attr)]
+# creating a new linear model without the variables that were linearly dependent
+dummy_model_2_outliers <- lm(SalePrice ~ ., data = dummy_df_2_outliers)
+summary(dummy_model_2_outliers)
+
+### using the previous model to predict the price of houses in the testing dataset
+dummy_vars <- c('BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'Fireplaces', 'GarageCars')
+dummy_df_test_2 <- dummy_cols(test_model_2_df, select_columns = dummy_vars, remove_selected_columns = TRUE)
+# attributes of the outlier linear model
+keep <- names(dummy_model_2_outliers$coefficients)[2:length(dummy_model_2_outliers$coefficients)]
+dummy_df_test_2 <- dummy_df_test_2[names(dummy_df_test_2) %in% keep]
+prediction <- predict(dummy_model_2_outliers, newdata = dummy_df_test_2)
+# store predicted house prices in the cleaned testing dataframe
+test_model_2_df$SalePrice <- prediction
+# plot the relationship between all variables and predicted sale price
+plotRelationship(test_model_2_df)
 
